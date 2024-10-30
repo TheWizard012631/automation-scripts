bcfec43d2184fe201e8498abd93ba1b488ca3a5e
TheWizard012631 <42426247+TheWizard012631@users.noreply.github.com>
2024-10-29 22:35:32 -0400

    Update linux-recyclebin.sh

--- linux-recyclebin.sh
+++ linux-recyclebin.sh
@@ -8,5 +8,5 @@ trash_bin=$(which trash);
 sudo mv $rm_dir/rm $rm_dir/unsafe-rm;
 sudo mv $rmdir_dir/rmdir $rmdir_dir/unsafe-rmdir;
 
-sudo ln -s $rm_dir/rm $trash_bin;
-sudo ln -s $rmdir_dir/rmdir $trash_bin;
+sudo ln -s $trash_bin $rm_dir/rm;
+sudo ln -s $trash_bin $rmdir_dir/rmdir;
